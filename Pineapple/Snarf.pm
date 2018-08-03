c46cabe6881bd7bcc54b97d09fb3a07503c16061
Stephen Ingram <frowey@gmail.com>
2018-08-03 09:20:58 -0400

    pineapple snarf (#14)

---
+++ Pineapple/Snarf.pm
@@ -0,0 +1,14 @@
+package Pineapple::Snarf;
+
+#use strict;
+
+use base qw( Exporter );
+our @EXPORT_OK = qw(
+	snarf
+);
+
+use constant {
+    snarf => "you put the lime in the coconut and drink it all up"
+};
+
+1;
